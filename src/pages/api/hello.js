5ceec11bbd3e31614f1efa84e4b463389b60674b
Edwin Vargas Ayala <88119497+Edwin3002@users.noreply.github.com>
2023-05-03 16:18:24 -0500

    Update hello.js

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,9 +1,11 @@
 import menuModel from "@/back/models/menuModel";
+import { startBackend } from "@/back/startBackend";
 
 
 
 export default async (req, res, next) => {
   // run morgan before the return response
+  startBackend();
   const data = await menuModel.find({ available: true }).limit(10);
   if (data[0]) return res.json({ code: 200, msg: "Busqueda exitosa", data: data });
   // return response to the client
