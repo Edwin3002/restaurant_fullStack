729e22b9b4fcb7b119f8f9e68458948534606b7b
Edwin Vargas Ayala <vargas11742@gmail.com>
2023-05-04 21:56:24 -0500

    change get orders

--- src/pages/api/orders/index.js
+++ src/pages/api/orders/index.js
@@ -2,6 +2,7 @@ import { startBackend } from "@/back/startBackend";
 import ordersModel from "@/back/models/ordersModel";
 import handlerValidator, { post, put, validateRequest } from "@/back/utils/handlerValidator";
 import { body, check, checkExact } from "express-validator";
+import { methods } from "@/back/utils/methods";
 
 startBackend();
 
@@ -53,7 +54,7 @@ const createUpdateOrderValidator = validateRequest([
     .notEmpty().withMessage("El campo esta vacio"),
   //"listOrder[*].price"
   check("listOrder[*].price")
-    .isInt({min: 999}).withMessage("El campo debe ser numerico y min. 1000")
+    .isInt({ min: 999 }).withMessage("El campo debe ser numerico y min. 1000")
     .exists().withMessage("El campo no existe")
     .notEmpty().withMessage("El campo esta vacio"),
   //listOrder[*].category
@@ -71,10 +72,61 @@ const createUpdateOrderValidator = validateRequest([
   checkExact([body('nameCustomer'), body('tableOrder'), body('amountOrder'), body('totalPriceOrder'), body('listOrder')], { message: "Los campos solo pueden ser [ nameCustomer, tableOrder, amountOrder, totalPriceOrder, listOrder ]" }),
 ]);
 
-export default handlerValidator.use(post(createUpdateOrderValidator), put(createUpdateOrderValidator))
-  .get(async (req, res) => {
+// export default handlerValidator.use(post(createUpdateOrderValidator), put(createUpdateOrderValidator))
+//   .get(async (req, res) => {
+//     try {
+//       let data;
+//       if (Object.values(req.body)[0]) {
+//         return res.status(404).json({ code: 404, msg: "No envies nada en el body" });
+//       }
+//       data = await ordersModel.find();
+//       if (data[0]) {
+//         return res.json({ code: 200, msg: "Busqueda exitosa", data: data });
+//       }
+//       return res.status(404).json({ code: 404, msg: "Busqueda fallida, no hay resgistros, intente otra pagina o la anterior" });
+//     } catch (err) {
+//       console.log(err, "fallo");
+//     }
+//     return res.status(404).json({ code: 404, msg: "Error inesperado, intente nuevamente", route: "orders, get" });
+//   })
+//   .post(async (req, res) => {
+//     try {
+//       const data = req.body;
+//       if (data._id) {
+//         return res.status(404).json({ code: 404, msg: "No envies un id, estas creando", route: "orders, post" });
+//       }
+//       await ordersModel.create(data);
+//       res.status(200).json({ code: 200, msg: "Creacion exitosa" });
+//       // io.emit("message", (await ordersModel.find()).reverse());
+//       return;
+//     } catch (err) {
+//       console.log("error" + err + "fallo la creacion");
+//     }
+//     return res.status(404).json({ code: 404, msg: "Error inesperado, intente nuevamente", route: "orders, post" });
+//   })
+//   .put(async (req, res) => {
+//     try {
+//       const data = req.body;
+//       const id = data._id;
+//       delete data._id;
+//       if (await ordersModel.findById(id)) {
+//         await ordersModel.findByIdAndUpdate(id, data);
+//         return res.status(200).json({ code: 200, msg: "Actualizacion exitosa" });
+//       }
+//       return res.status(404).json({ code: 404, msg: "El ID no se encuentra registrado", route: "orders, put" });
+//     } catch (error) {
+//       console.log("error" + error + "fallo la actualizacion");
+//     }
+//     return res.status(404).json({ code: 404, msg: "Error inesperado, intente nuevamente", route: "orders, psot" });
+//   })
+
+
+
+export default async function handler(req, res) {
+  const method = req.method;
+  if (methods.GET === method) {
     try {
-    let data;
+      let data;
       if (Object.values(req.body)[0]) {
         return res.status(404).json({ code: 404, msg: "No envies nada en el body" });
       }
@@ -86,9 +138,7 @@ export default handlerValidator.use(post(createUpdateOrderValidator), put(create
     } catch (err) {
       console.log(err, "fallo");
     }
-    return res.status(404).json({ code: 404, msg: "Error inesperado, intente nuevamente", route: "orders, get" });
-  })
-  .post(async (req, res) => {
+  } else if (methods.POST === method) {
     try {
       const data = req.body;
       if (data._id) {
@@ -101,9 +151,7 @@ export default handlerValidator.use(post(createUpdateOrderValidator), put(create
     } catch (err) {
       console.log("error" + err + "fallo la creacion");
     }
-    return res.status(404).json({ code: 404, msg: "Error inesperado, intente nuevamente", route: "orders, post" });
-  })
-  .put(async (req, res) => {
+  } else if (methods.PUT === method) {
     try {
       const data = req.body;
       const id = data._id;
@@ -116,5 +164,7 @@ export default handlerValidator.use(post(createUpdateOrderValidator), put(create
     } catch (error) {
       console.log("error" + error + "fallo la actualizacion");
     }
-    return res.status(404).json({ code: 404, msg: "Error inesperado, intente nuevamente", route: "orders, psot" });
-  })
+  }
+
+  return res.status(404).json({ code: 404, msg: "Error inesperado, intente nuevamente", route: "orders, get" });
+}
